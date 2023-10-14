28d51857b3259d0c70bcdcdfde0de51f224320ff
Bornieque Pacheco <bornieque@gmail.com>
2023-10-14 20:38:17 -0300

    Correção erro Resource leak: 'in' is never closed

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,30 +1,32 @@
+import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // Conhecer e importar a classe Scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Por favor, digite o número da sua conta!");
+            int numero = scanner.nextInt();
 
-        System.out.println("Por favor, digite o número da sua conta!");
-        int numero = scanner.nextInt();
+            System.out.println("Por favor, digite o número da sua Agência!");
+            String agencia = scanner.next();
 
-        System.out.println("Por favor, digite o número da sua Agência!");
-        String agencia = scanner.next();
+            System.out.println("Por favor, digite o seu primeiro nome");
+            String nomeCliente = scanner.next();
 
-        System.out.println("Por favor, digite o seu primeiro nome");
-        String nomeCliente = scanner.next();
+            System.out.println("Por favor, digite o seu ultimo nome");
+            String sobreNomeCliente = scanner.next();
 
-        System.out.println("Por favor, digite o seu ultimo nome");
-        String sobreNomeCliente = scanner.next();
+            System.out.println("Por favor, digite o seu saldo");
+            double saldo = scanner.nextDouble();
 
-        System.out.println("Por favor, digite o seu saldo");
-        double saldo = scanner.nextDouble();
+            System.out.println("Olá " + nomeCliente + " " + sobreNomeCliente
+                    + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                    + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        } finally {
 
-        System.out.println("Olá " + nomeCliente + " " + sobreNomeCliente
-                + ", obrigado por criar uma conta em nosso banco, sua agência é "
-                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
-
+        }
     }
 }
